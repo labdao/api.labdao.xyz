241892e9e0c720f324b8f0b4dbd1777a3935184d
Jesse Evers <jesse@jesseevers.com>
2022-12-18 17:18:49 -0500

    Remove configService from orgconfig.ts

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -1,18 +1,12 @@
 import { DataSource } from 'typeorm';
-import { ConfigService } from '@nestjs/config';
-import { config } from 'dotenv';
-
-config();
-
-const configService = new ConfigService();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: configService.get('DB_HOST'),
-  port: configService.get('DB_PORT'),
-  username: configService.get('DB_USERNAME'),
-  password: configService.get('DB_PASSWORD'),
-  database: configService.get('DB_DATABASE'),
+  host: '127.0.0.1',
+  port: 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: false,
   logging: false,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
