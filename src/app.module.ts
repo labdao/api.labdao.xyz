a5afd8c24d0496d16e182bde9b4a96ec9507bc73
Jesse Evers <jesse@jesseevers.com>
2022-12-19 14:18:52 -0500

    Add ConfigModule back to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
@@ -13,13 +14,14 @@ import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
       port: 5432,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      database: process.env.DB_NAME,
       entities: [User, Project, Skill],
       synchronize: false,
     }),
