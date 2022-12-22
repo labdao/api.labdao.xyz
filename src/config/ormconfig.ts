e5321ca0cda7278b419ce8dcdd4646cdc5eea33f
Victor Bordo <victorbordo@users.noreply.github.com>
2022-12-22 10:23:10 -0500

    Create split db config for typeorm between dev and prod.

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -1,14 +1,35 @@
 import { DataSource } from 'typeorm';
 
-export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: '127.0.0.1',
-  port: 5432,
+let entitiesPath: string;
+const dbConfig = {
+  host: process.env.DB_HOST,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+};
+
+if (process.env.NODE_ENV == 'production') {
+  entitiesPath = '/../**/*.entity.js';
+} else {
+  import('dotenv').then((dotenv) => {
+    dotenv.config();
+    dbConfig.host = process.env.DB_HOST;
+    dbConfig.username = process.env.DB_USERNAME;
+    dbConfig.password = process.env.DB_PASSWORD;
+    dbConfig.database = process.env.DB_NAME;
+  });
+  entitiesPath = '/../**/*.entity.{js,ts}';
+}
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: dbConfig.host,
+  port: 5432,
+  username: dbConfig.username,
+  password: dbConfig.password,
+  database: dbConfig.database,
   synchronize: false,
   logging: false,
-  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  entities: [__dirname + entitiesPath],
   migrations: ['src/migrations/*.ts'],
 });
